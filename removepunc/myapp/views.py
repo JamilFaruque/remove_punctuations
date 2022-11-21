3ec354e6c4e576502b5d1346c442f3c8b8183b7f
JamilFaruque <jamilfaruk7@gmail.com>
2022-11-22 02:45:48 +0600

    second commit

--- removepunc/myapp/views.py
+++ removepunc/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
@@ -8,11 +8,35 @@ def home(request):
 
 def analyze(request):
     result = ""
+    t = ""
     punc = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
 
     txt = request.GET.get('text')
-    for char in txt:
-        if char not in punc:
-            result = result + char
+    removepunc = request.GET.get('removepunc', 'off')
+    allcaps = request.GET.get('allcaps','off')
+    charcount = request.GET.get('charcount','off')
+
+    if removepunc == 'on' and allcaps == 'on':
+
+        for char in txt:
+            if char not in punc:
+                result = result + char.upper()
+
+    elif removepunc == 'on':
+        for char in txt:
+            if char not in punc:
+                result = result + char
+
+    elif allcaps == 'on':
+        for char in txt:
+            result = result + char.upper()
+
+    elif charcount == 'on':
+        for char in txt:
+            if char != ' ':
+                t = t + char
+                result = len(t)
+    else:
+        return HttpResponse('PLEASE CHOOSE AN OPTION')
     param = {'analyzed': result }
     return render(request, 'result.html', param )
