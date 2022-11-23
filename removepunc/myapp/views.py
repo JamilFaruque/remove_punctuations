24bc9e36f5444948ee74577e14498cb8fd7bcda9
JamilFaruque <jamilfaruk7@gmail.com>
2022-11-23 17:52:34 +0600

    last commit

--- removepunc/myapp/views.py
+++ removepunc/myapp/views.py
@@ -7,36 +7,41 @@ def home(request):
     return render(request,'home.html')
 
 def analyze(request):
-    result = ""
     t = ""
     punc = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
-
+    count = ""
     txt = request.GET.get('text')
     removepunc = request.GET.get('removepunc', 'off')
     allcaps = request.GET.get('allcaps','off')
     charcount = request.GET.get('charcount','off')
 
-    if removepunc == 'on' and allcaps == 'on':
+    # if removepunc == 'on' and allcaps == 'on':
 
-        for char in txt:
-            if char not in punc:
-                result = result + char.upper()
+    #     for char in txt:
+    #         if char not in punc:
+    #             result = result + char.upper()
 
-    elif removepunc == 'on':
+    if removepunc == 'on':
+        result = ""
         for char in txt:
             if char not in punc:
                 result = result + char
+        txt = result
 
-    elif allcaps == 'on':
+    if allcaps == 'on':
+        result = ""
         for char in txt:
             result = result + char.upper()
+        txt = result
 
-    elif charcount == 'on':
+    if charcount == 'on':
+           
         for char in txt:
             if char != ' ':
                 t = t + char
-                result = len(t)
-    else:
-        return HttpResponse('PLEASE CHOOSE AN OPTION')
-    param = {'analyzed': result }
+                count = "---" + str(len(t)) + " characters "
+        
+    if removepunc == 'off' and allcaps=='off' and charcount=='off':
+        return HttpResponse("PLEASE CHOOSE AN OPTION")
+    param = {'analyzed': result , 'count':count }
     return render(request, 'result.html', param )
